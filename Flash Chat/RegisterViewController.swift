322a32f62f6991af07d566f041c11d620bada417
Evangelos Konstantinidis <evangelos.konstantinidis@exxeta.com>
2018-04-02 23:24:10 +0200

    Navigation Controllers, Login Screen

--- Flash Chat/RegisterViewController.swift
+++ Flash Chat/RegisterViewController.swift
@@ -10,10 +10,10 @@ import Firebase
 
 
 class RegisterViewController: UIViewController {
-
+    
     
     //Pre-linked IBOutlets
-
+    
     @IBOutlet var emailTextfield: UITextField!
     @IBOutlet var passwordTextfield: UITextField!
     
@@ -21,19 +21,20 @@ class RegisterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-
-  
+    
+    
     @IBAction func registerPressed(_ sender: AnyObject) {
         
-
+        
         
         //TODO: Set up a new user on our Firbase database
-        Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) { (user, error) in
+        Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) {
+            (user, error) in
             if error != nil {
                 print(error!)
             } else {
@@ -44,7 +45,7 @@ class RegisterViewController: UIViewController {
             }
         }
         
-
+        
         
         
     } 
